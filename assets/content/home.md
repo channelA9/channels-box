8c22ba60b3b4b9150c75a2b6c5a2fd94fff56e68
channel9A <shaun.colegado@gmail.com>
2024-10-04 23:47:05 -0700

    more gifs

--- assets/content/home.md
+++ assets/content/home.md
@@ -6,4 +6,3 @@ This website is where you can find a list of my [projects](/projects) and read p
 
 
 Beyond that, though, there's not much to see here. 
-![](/images/computer-sleep-0822.gif)
--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -7,6 +7,11 @@ import content from '../../assets/content/home.md?raw'
 export default function Page() {
   return (
     <>
+      <div class="flex">
+        <img src="/images/pikachu-0008.gif" />
+        <img src="/images/pikachu-0008.gif" />
+        <img src="/images/pikachu-0008.gif" />
+      </div>
       <article class="prose  min-w-full">
         <SolidMarkdown>{content}</SolidMarkdown>
       </article>
